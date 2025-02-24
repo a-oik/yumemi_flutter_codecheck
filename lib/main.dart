bea884f4727be6523b390eaadb6d6dfdb2847cd0
尾池亜紀 <fogfrog1214@gmail.com>
2025-02-25 03:03:24 +0900

    #11 ダークモード対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yumemi_flutter_codecheck/l10n/l10n.dart';
+import 'package:yumemi_flutter_codecheck/provider/is_dark_mode_provider.dart';
 import 'package:yumemi_flutter_codecheck/view/github_search_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: const GitHubSearchApp()));
 }
 
-class GitHubSearchApp extends StatefulWidget {
+class GitHubSearchApp extends ConsumerStatefulWidget {
   const GitHubSearchApp({super.key});
 
   @override
   createState() => _GitHubSearchAppState();
 }
 
-class _GitHubSearchAppState extends State<GitHubSearchApp> {
+class _GitHubSearchAppState extends ConsumerState<GitHubSearchApp> {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = ref.watch(isDarkModeProvider);
     return MaterialApp(
+      // テーマ切り替え
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
       localizationsDelegates: L10n.localizationsDelegates,
       supportedLocales: L10n.supportedLocales,
-      // todo: Dark Mode対応
       home: GithubSearchScreen(),
     );
   }
