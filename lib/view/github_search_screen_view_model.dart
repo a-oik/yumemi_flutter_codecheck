7ce0d74b606f40eb1585becff68312ff738970d6
尾池亜紀 <fogfrog1214@gmail.com>
2025-02-24 22:55:48 +0900

    #9 検索画面のviewmodel層の作成(usecase層は不要だったため削除)

--- lib/view/github_search_screen_view_model.dart
+++ lib/view/github_search_screen_view_model.dart
@@ -1,19 +1,23 @@
 import 'package:yumemi_flutter_codecheck/repository/github_repository.dart';
-import 'package:yumemi_flutter_codecheck/use_case/get_repository_info_use_case.dart';
 
 import '../model/api_result.dart';
 import '../model/repo_search_input.dart';
 import '../model/repository_info.dart';
 
 class GithubSearchScreenViewModel {
-  GithubSearchScreenViewModel(GetRepoInfoUseCase? getRepoInfoUseCase) {
-    _getRepoInfoUseCase =
-        getRepoInfoUseCase ?? GetRepoInfoUseCase(GithubSearchRepository());
+  late GithubSearchRepository _githubRepository;
+
+  GithubSearchScreenViewModel({GithubSearchRepository? githubRepo}) {
+    _githubRepository = githubRepo ?? GithubSearchRepository();
   }
 
-  late GetRepoInfoUseCase _getRepoInfoUseCase;
-
-  Future<ApiResult<RepoInfo>> getRepoInfo({required RepoSearchInput input}) {
-    return _getRepoInfoUseCase.getRepoInfo(input: input);
+  // repositoryの検索を行う
+  Future<ApiResult<RepoInfo>> getRepoInfo({
+    required RepoSearchInput input,
+  }) async {
+    return await _githubRepository.getRepoInfo(
+      searchText: input.searchText,
+      sort: input.sort,
+    );
   }
 }
