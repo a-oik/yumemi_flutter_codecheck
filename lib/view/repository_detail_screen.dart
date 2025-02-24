1baa15d29c082858d4c1e0536b312cf0d26156f0
尾池亜紀 <fogfrog1214@gmail.com>
2025-02-25 02:14:21 +0900

    #15 詳細画面をスクロール可能にする 万が一説明文(description)が画面に収まりきらない場合に備えている

--- lib/view/repository_detail_screen.dart
+++ lib/view/repository_detail_screen.dart
@@ -33,44 +33,46 @@ class RepositoryDetailScreen extends StatelessWidget {
               color: false ? Colors.black : Colors.white,
               borderRadius: BorderRadius.all(Radius.circular(8.0)),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // 作者アイコン & repository名
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 64.0,
-                      height: 64.0,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        backgroundImage: NetworkImage(repo.owner.avatarUrl),
-                      ),
-                    ),
-                    SizedBox(width: 8.0),
-                    Expanded(
-                      child: Text(
-                        repo.fullName,
-                        style: TextStyle(
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.w500,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 作者アイコン & repository名
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 64.0,
+                        height: 64.0,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: NetworkImage(repo.owner.avatarUrl),
                         ),
-                        maxLines: 2,
                       ),
-                    ),
-                  ],
-                ),
-                // 説明文
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Text(repo.description ?? ""),
-                ),
-                // アイコン群
-                RepoInfoIconList(repo: repo),
-                SizedBox(height: 12.0),
-                // todo: 「Githubで開く」ボタンの実装
-              ],
+                      SizedBox(width: 8.0),
+                      Expanded(
+                        child: Text(
+                          repo.fullName,
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 説明文
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Text(repo.description ?? ""),
+                  ),
+                  // アイコン群
+                  RepoInfoIconList(repo: repo),
+                  SizedBox(height: 12.0),
+                  // todo: 「Githubで開く」ボタンの実装
+                ],
+              ),
             ),
           ),
           // 閉じる(×)ボタン
